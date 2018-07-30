6eed6e6adfa17210aa1dadcd4269b1310900696e
rfrancou <francou_4@hotmail.com>
2018-07-29 21:00:49 -0300

    Registered suppliers in an item

--- routes/api/rubros/index.js
+++ routes/api/rubros/index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const controller = require('./proveedores/registrados.controller')
 const router = express.Router()
 
+router.get('/:id/proveedores/registrados', controller.proveedoresRegistradosRubro)
 router.get('/proveedores/registrados', controller.proveedoresRegistradosRubros)
+
 
 module.exports = router
--- routes/api/rubros/proveedores/registrados.controller.js
+++ routes/api/rubros/proveedores/registrados.controller.js
@@ -2,12 +2,23 @@
 
 const router = require('express').Router()
 const utils = require(__base + '/scrapper')
-const urlBase = 'http://www.cdeluruguay.gob.ar/datagov/catalogoRubrosProveedores'
+const urlBase = 'http://www.cdeluruguay.gob.ar/datagov/'
 
 exports.proveedoresRegistradosRubros = (req, res, next) => {
-    let url = `${urlBase}.php`
+    let url = `${urlBase}catalogoRubrosProveedores.php`
     utils.scrapper(url, true).then((table) => {
         res.json({rubros: table})
+    }, (err) => {
+        res.status(404).send({error: err})
+    })
+}
+
+exports.proveedoresRegistradosRubro = (req, res, next) => {
+    let id = parseInt(req.params.id)
+    let url = `${urlBase}proveedoresPorRubro.php?idRubro=${id}`
+    // .php?idRubro=39
+    utils.scrapper(url, false).then((table) => {
+        res.json({proveedores: table})
     }, (err) => {
         res.status(404).send({error: err})
     })
