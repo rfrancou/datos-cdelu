93a013bc6a581f0167c863e4f536fc34504ce6db
rfrancou <francou_4@hotmail.com>
2019-09-08 19:05:31 -0300

    Add middleware to main route

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ global.__base = __dirname + '/';
 const express = require('express')
 const logger = require('morgan')
 const path = require('path')
+const cacheUtils = require("./cache-utils")
 
 let app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(logger('dev'))
 
-app.use('/api/v1', require('./routes/api'))
+app.use('/api/v1', cacheUtils.cache(30000), require('./routes/api'))
 
 app.listen(app.get('port'), () => {
   console.log(`Running on PORT --> ${app.get('port')}`)
