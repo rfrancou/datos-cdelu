42305d5f6e8fa6df3e45658a0ad2bb7d6fc546c7
rfrancou <francou_4@hotmail.com>
2019-09-09 20:59:53 -0300

    TTL value from environment varible and set default value to 12hs

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const express = require('express')
 const logger = require('morgan')
 const path = require('path')
 const cacheUtils = require("./cache-utils")
-
+const ttl = process.env.TTL || 43200000
 let app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(logger('dev'))
 
-app.use('/api/v1', cacheUtils.cache(30000), require('./routes/api'))
+app.use('/api/v1', cacheUtils.cache(ttl), require('./routes/api'))
 
 app.listen(app.get('port'), () => {
   console.log(`Running on PORT --> ${app.get('port')}`)
